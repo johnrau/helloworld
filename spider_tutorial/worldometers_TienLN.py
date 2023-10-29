b31da8119698294100182c35f1bb8b433ee1e1d7
johnrau <luc.thang@gmail.com>
2023-10-29 17:55:43 +0700

    first commit

---
+++ spider_tutorial/worldometers_TienLN.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class WorldometersSpider(scrapy.Spider):
+    name = "worldometers"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+
+    def parse(self, response):
+        countries = response.xpath('//td/a')
+        
+        for country in countries :
+            country_name = country.xpath('.//text()').get()
+            link = country.xpath('.//@href').get() 
+            yield response.follow(url=link, callback=self.parse_country())
+
+    def parse_country(self, response):
+        rows = response.xpath("(//table[contains(@class,'table')])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+        yield {
+            'year':year,
+            'population':population,
+        }    
